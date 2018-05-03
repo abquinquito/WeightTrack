15552b4e26fcbba65cea894f96129ae7891ecff6
RayAdrian <raymund.adrian.rafael@gmail.com>
2018-05-04 03:29:32 +0800

    Added: Bottom bar, Fragments, Graphing, Settings Page

---
+++ app/src/main/java/ph/edu/upd/eee/weighttrack/Settingsfragment.java
@@ -0,0 +1,25 @@
+package com.example.raymundrafael.weighttrak;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by raymundrafael on 30/04/2018.
+ */
+
+public class Settingsfragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.settings,container,false);
+        return v;
+    }
+
+
+}
